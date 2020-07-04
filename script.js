ce86e82708b36123c9443f3e78576ff0a565b2da
unknown <amankmchoudhary@gmail.com>
2020-07-04 12:19:56 +0530

    add some budles and modules in it

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:' , _.without(array, 3));
+
+
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
@@ -9,21 +15,21 @@ function inputLength() {
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);	
+	ul.appendChild(li);
 	input.value = "";
 }
 function addListAfterClick() {
-		if (inputLength() > 0) {
-	createListElement();
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
+function addListAfterKeypress(event) {
+	if (inputlength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
-function addListAfterKeypress(event){
-		if (inputlength() > 0 && event.keyCode === 13) {
-	createListElement();
-}
-}
-button.addEventListener("click",addListAfterClick);
-	
+button.addEventListener("click", addListAfterClick);
+
 
 input.addEventListener("keypress", addListAfterKeypress);
-	
+
